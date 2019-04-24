6e0ce65ac993f504ee493e365a1e9f92f9ebf057
Kevin Heis <heiskr@users.noreply.github.com>
2019-04-24 07:51:02 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,23 +3,31 @@ var bs58 = require('bs58')
 var REMOVE_DASH_REGEXP = /-/g
 
 function convertUuidToUuid58(uuid) {
-  return bs58.encode(
-    Buffer.from(
-      uuid.replace(REMOVE_DASH_REGEXP, ''),
-      'hex'
-    )
-  )
+  try {
+    return bs58.encode(
+      Buffer.from(
+        uuid.replace(REMOVE_DASH_REGEXP, ''),
+        'hex'
+      )
+    ) 
+  } catch (e) {
+    return uuid 
+  }
 }
 
 function convertUuid58ToUuid(uuid58) {
-  var hex = bs58.decode(uuid58).toString('hex')
-  return [
-    hex.substr(0, 8),
-    hex.substr(8, 4),
-    hex.substr(12, 4),
-    hex.substr(16, 4),
-    hex.substr(20)
-  ].join('-')
+  try {
+    var hex = bs58.decode(uuid58).toString('hex')
+    return [
+      hex.substr(0, 8),
+      hex.substr(8, 4),
+      hex.substr(12, 4),
+      hex.substr(16, 4),
+      hex.substr(20)
+    ].join('-')
+  } catch (e) {
+    return uuid58 
+  }
 }
 
 module.exports = {
